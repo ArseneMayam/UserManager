9cbc93ae52db5f58d04d03093544ba8c8509abc4
Arsene Mayam <arsene.mayam@cgi.com>
2020-06-02 10:03:14 -0400

    added mock endpoints_ working

--- usermanagement/Controllers/DefaultController.cs
+++ usermanagement/Controllers/DefaultController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using UserManager.Common.Models;
 
 namespace UserManager.Api.Controllers
 {
@@ -17,6 +18,24 @@ namespace UserManager.Api.Controllers
         public ActionResult<string> Version()
         {
             return "Version 1.0.0 !!!!";
+        }
+
+        [HttpGet]
+        [Route("employe")]
+        public ActionResult<Employe> Employe()
+        {
+            return new Employe()
+            {
+                CodeEmpl = 123,
+                EmployeId = 01,
+                Matricule = "LT46",
+                Nas = 946,
+                Nationalite = "Canada",
+                Nom = "Dennis",
+                Prenom = "Lemaire",
+                Rib = "LAS976",
+                Tin = "SA9"
+            };
         }
     }
 }
