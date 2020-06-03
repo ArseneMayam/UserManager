5e7ec848bc488f563dd1ce3a0b73b4cf6209adea
Arsene Mayam <arsene.mayam@cgi.com>
2020-06-03 13:25:26 -0400

    update DataAccess. build success

--- UserManager.Services/Source/DataAccessService.cs
+++ UserManager.Services/Source/DataAccessService.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Text;
 using UserManager.Common.Models;
 using UserManager.Data;
+using UserManager.Data.Interfaces;
 using UserManager.Services.Interfaces;
 namespace UserManager.Services.Source
 {
     public class DataAccessService : IDataAccesService
     {
+        internal IProfileData ProfileData { get; set; }
+        internal IUtilisateurData UtilisateurData { get; set; }
+        internal IColonneData ColonneData { get; set; }
+        internal IProfileColonneData ProfileColonneData { get; set; } 
+        public DataAccessService(IProfileData profileData, IUtilisateurData utilisateurData, IColonneData colonneData, IProfileColonneData profileColonneData)
+        {
+            ProfileData = profileData;
+            UtilisateurData = utilisateurData;
+            ColonneData = colonneData;
+            ProfileColonneData = profileColonneData;
+
+        }
         // Methodes pour gerer data access 
         // Avec credentials
         public IList<Colonne> GererDataAccess(string username, string password)
@@ -21,24 +34,24 @@ namespace UserManager.Services.Source
         }
 
         // Recuperer tous les colonnes avec la liste des colonnes_id       
-        public IList<Colonne> RecupererColonnes(IList<int> colonne_id)
+        public IList<Colonne> RecupererColonnes(IList<int> listeColonne_id)
         {
-            throw new NotImplementedException();
+            return ColonneData.RecupererColonnes(listeColonne_id);
         }
         // Recuperer la liste des colonne_id avec un profile_id
         public IList<int> RecupererListeColonneIds(int profile_id)
         {
-            throw new NotImplementedException();
+            return ProfileColonneData.RecupererListeColonneIds(profile_id);
         }
         // Recuperer profile de l'utilisateur     
         public Profile RecupererProfile(int utlisateur_id)
         {
-            throw new NotImplementedException();
+            return ProfileData.RecupererProfile(utlisateur_id);
         }
         // Recuperer utilisateur
         public Utilisateur RecupererUtilisateur(string username, string password)
         {
-            return null;
+            return UtilisateurData.RecupererUtilisateur(username,password);
         }
     }
 }
